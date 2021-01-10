77508becc07540e17de7500006ceb91933916a9f
Kevin Ivan Sanchez Valdin <62733314+KevinSanchez-Cat@users.noreply.github.com>
2021-01-09 21:52:46 -0600

    Añadir el JTatto

--- src/primerossiguientes/Principa.java
+++ src/primerossiguientes/Principa.java
@@ -7,21 +7,28 @@ package primerossiguientes;
 
 import javax.swing.JFrame;
 import javax.swing.JOptionPane;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 /**
  *
  * @author por_s
  */
 public class Principa extends JFrame {
-    
+
     public static void main(String[] args) {
+
         try {
+            try {
+                UIManager.setLookAndFeel("com.jtattoo.plaf.noire.NoireLookAndFeel");
+            } catch (ClassNotFoundException | IllegalAccessException | InstantiationException | UnsupportedLookAndFeelException e) {
+            }
             Sintactico s = new Sintactico();
             s.setVisible(true);
         } catch (Exception e) {
             JOptionPane.showMessageDialog(null, "Error fatal", "Mensaje de Error", JOptionPane.ERROR_MESSAGE);
             System.exit(0);
         }
-        
+
     }
 }
